c32fe569e780bcd1ec09cb04873853ed22fe7443
crabemoji <naisyboy@gmail.com>
2024-10-20 22:08:04 +0500

    Create the Keychooser tool
    Keychooser selects or deselects only every Xth frame in the dope sheet

--- keyfiller.py
+++ keyfiller.py
@@ -3,7 +3,7 @@ import bpy
 bl_info = {
     "name": "Keyfiller",
     "author": "crab emoji",
-    "version": (0, 1),
+    "version": (0, 2),
     "blender": (3, 6, 2),
     "location": "Dope Sheet > Key > Keyfill",
     "description": "Inserts keyframes on all channels for every keyframe on any other channel",
@@ -13,6 +13,66 @@ bl_info = {
 }
 
 
+
+class KEYCHOOSER_MT(bpy.types.Operator):
+    """Select only keyframes on certain timings (2's, 3's, etc.)"""
+    bl_idname = "keyfiller.keychooser"
+    bl_label = "Keychoose"
+    bl_options = {'REGISTER', 'UNDO'}
+    
+    count_x: bpy.props.IntProperty(
+        name = "Every X frames",
+        description = "Select only every Xth frame",
+        default = 2
+    )
+    offset_x: bpy.props.IntProperty(
+        name = "Offset",
+        description = "Starting frame",
+        default = 0
+    )
+    deselect_instead: bpy.props.BoolProperty(
+        name = "Invert selection",
+        description = "Deselects every Xth frame",
+        default = False
+    )
+    
+    def execute(self, context):
+        bpy.context.area.type = 'DOPESHEET_EDITOR'
+        
+        # 1. Remember the current frame & deselect all keyframes
+        _current_frame_prev = context.scene.frame_current
+        _action = 'DESELECT'
+        bpy.ops.action.select_all(action = _action)
+        
+        # 2. Select keyframes on current frame once every X frames
+        X = self.count_x
+        frame_start = context.scene.frame_start + self.offset_x
+        for frame in range(frame_start, context.scene.frame_end + 1):
+            X += 1
+            
+            if not self.deselect_instead:
+                if X >= self.count_x:
+                    frame_select_current(context, frame)
+                    
+                    X = 0
+            else:
+                if X < self.count_x:
+                    frame_select_current(context, frame)
+                else:
+                    X = 0
+                
+        
+        bpy.ops.screen.frame_current = _current_frame_prev
+        
+        return {'FINISHED'}
+    
+    def menu_item(self, context):
+        self.layout.operator(KEYCHOOSER_MT.bl_idname, icon='ACTION_TWEAK')
+        
+def frame_select_current(context, frame):
+    context.scene.frame_current = frame
+    _mode = 'CFRA'
+    bpy.ops.action.select_column(mode = _mode)
 
 
 class KEYFILLER_MT(bpy.types.Operator):
@@ -62,7 +122,8 @@ class KEYFILLER_MT(bpy.types.Operator):
 
 
 classes = {
-    KEYFILLER_MT
+    KEYFILLER_MT,
+    KEYCHOOSER_MT
 }
 
 def register():
